52963e2e7c867e8c381948276bef2618df7602b5
Fabien <fabienmicallef@gmail.com>
2025-09-05 16:56:45 +0200

    update

--- liquid/app.py
+++ liquid/app.py
@@ -1,8 +1,17 @@
 from liquid import Environment, CachingFileSystemLoader
+from lxml import etree
+import html5lib
 
 env = Environment(loader=CachingFileSystemLoader("templates"))
-template = env.get_template("index.liquid")
 
-html = template.render(name="Fabien", page_title="Home")
+# Render your Liquid template
+template = env.get_template("index.liquid")
+raw_html = template.render(name="Fabien", page_title="Home")
+
+# Parse and beautify the HTML -> TODO at the end to fix everything
+# document = html5lib.parse(raw_html, treebuilder="lxml", namespaceHTMLElements=False)
+# pretty_html = etree.tostring(document, pretty_print=True, encoding="unicode")
+
 with open("output.html", "w") as f:
-    f.write(html)
+    #f.write(pretty_html)    
+    f.write(raw_html)
