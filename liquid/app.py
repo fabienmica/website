e8347df47c22f0521d18b644ca9fb4925d0fcb91
Fabien <fabienmicallef@gmail.com>
2025-09-13 13:11:33 +0200

    update

--- liquid/app.py
+++ liquid/app.py
@@ -2,7 +2,7 @@ from liquid import Environment, CachingFileSystemLoader
 from lxml import etree
 import html5lib
 
-def generateIndex(env, name, title):
+def generateIndex(env, name, title, file):
     # Render your Liquid template
     template = env.get_template(name + ".liquid")
     raw_html = template.render(title=title)
@@ -11,11 +11,11 @@ def generateIndex(env, name, title):
     # document = html5lib.parse(raw_html, treebuilder="lxml", namespaceHTMLElements=False)
     # pretty_html = etree.tostring(document, pretty_print=True, encoding="unicode")
 
-    with open("generated/" + name + ".html", "w") as f:
+    with open("generated/" + file, "w") as f:
         #f.write(pretty_html)    
         f.write(raw_html)
 
 env = Environment(loader=CachingFileSystemLoader("templates"))
 
-generateIndex(env, "index", title="")
-generateIndex(env, "portfolio-enwaii", title="Enwaii")
+generateIndex(env, "index", title="Fabien Micallef", file="index.html")
+generateIndex(env, "portfolio-details", title="Enwaii", file="portfolio-enwaii.html")
