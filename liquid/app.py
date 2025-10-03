7a2a86880cd11540e0ff8d1a82f91d06a73811cb
Fabien <fabienmicallef@gmail.com>
2025-10-03 09:37:28 +0200

    update

--- liquid/app.py
+++ liquid/app.py
@@ -3,7 +3,7 @@ from lxml import etree
 import html5lib
 import shutil
 
-def generateIndex(env, name, title, file):
+def generateIndex(env, name, file, title):
     # Render your Liquid template
     template = env.get_template(name + ".liquid")
     raw_html = template.render(title=title)
@@ -20,12 +20,17 @@ def generateIndex(env, name, title, file):
     #Copy file directly
     shutil.copy(file_path_generated, "../alquanti")
 
+# Load data
+#with open("data/buf_projects.json") as f:
+#    buf_projects = json.load(f)
+
+
 env = Environment(loader=CachingFileSystemLoader("templates"))
 
-generateIndex(env, "index", title="Fabien Micallef", file="index.html")
-generateIndex(env, "portfolio-details", title="Enwaii", file="portfolio-enwaii.html")
-generateIndex(env, "portfolio-details", title="Hold The World", file="portfolio-hold-the-world.html")
-generateIndex(env, "portfolio-details", title="VRsatile", file="portfolio-vrsatile.html")
-generateIndex(env, "portfolio-details", title="Pioneering Reality Capture", file="portfolio-pioneering.html")
-generateIndex(env, "portfolio-details", title="Immersive projects", file="portfolio-immersive.html")
-generateIndex(env, "portfolio-details", title="Commercials", file="portfolio-commercials.html")
+generateIndex(env, "index", file="index.html", title="Fabien Micallef")
+generateIndex(env, "portfolio-details", file="portfolio-enwaii.html", title="Enwaii")
+generateIndex(env, "portfolio-details", file="portfolio-hold-the-world.html", title="Hold The World")
+generateIndex(env, "portfolio-details", file="portfolio-vrsatile.html", title="VRsatile")
+generateIndex(env, "portfolio-details", file="portfolio-pioneering.html", title="Pioneering Reality Capture")
+generateIndex(env, "portfolio-details", file="portfolio-immersive.html", title="Immersive projects")
+generateIndex(env, "portfolio-details", file="portfolio-commercials.html", title="Commercials")
