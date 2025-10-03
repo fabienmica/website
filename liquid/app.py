433c0e9766acda703e4c18aaf073f04634599098
Fabien <fabienmicallef@gmail.com>
2025-10-03 10:32:06 +0200

    swipper

--- liquid/app.py
+++ liquid/app.py
@@ -1,12 +1,13 @@
+import json
 from liquid import Environment, CachingFileSystemLoader
 from lxml import etree
 import html5lib
 import shutil
 
-def generateIndex(env, name, file, title):
+def generateIndex(env, name, file, **kwargs):
     # Render your Liquid template
     template = env.get_template(name + ".liquid")
-    raw_html = template.render(title=title)
+    raw_html = template.render(**kwargs)
 
     # Parse and beautify the HTML -> TODO at the end to fix everything
     #document = html5lib.parse(raw_html, treebuilder="lxml", namespaceHTMLElements=False)
@@ -21,8 +22,8 @@ def generateIndex(env, name, file, title):
     shutil.copy(file_path_generated, "../alquanti")
 
 # Load data
-#with open("data/buf_projects.json") as f:
-#    buf_projects = json.load(f)
+with open("data/buf_projects.json") as f:
+    buf_projects = json.load(f)
 
 
 env = Environment(loader=CachingFileSystemLoader("templates"))
@@ -31,6 +32,6 @@ generateIndex(env, "index", file="index.html", title="Fabien Micallef")
 generateIndex(env, "portfolio-details", file="portfolio-enwaii.html", title="Enwaii")
 generateIndex(env, "portfolio-details", file="portfolio-hold-the-world.html", title="Hold The World")
 generateIndex(env, "portfolio-details", file="portfolio-vrsatile.html", title="VRsatile")
-generateIndex(env, "portfolio-details", file="portfolio-pioneering.html", title="Pioneering Reality Capture")
+generateIndex(env, "portfolio-details", file="portfolio-pioneering.html", title="Pioneering Reality Capture", buf_projects=buf_projects)
 generateIndex(env, "portfolio-details", file="portfolio-immersive.html", title="Immersive projects")
 generateIndex(env, "portfolio-details", file="portfolio-commercials.html", title="Commercials")
